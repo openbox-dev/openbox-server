727f37eaeb485757daebd276466c8878f8a0a58f
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-16 23:36:24 +0100

    feat(header): add responsiveness to header handled viewpoint 1110px still need modal menu

--- app/ui/common/Header/Header.tsx
+++ app/ui/common/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { Link } from "@remix-run/react";
 import MediumLogo from "../../../assets/medium-logo.svg";
+import SmallLogo from "../../../assets/small-logo.svg";
+import BurgerIcon from "../../../assets/burger.svg";
 
 export default function Header() {
   return (
     <div className="Header">
       <nav>
-        <img src={MediumLogo} alt="OpenBox logo" />
+        <img src={MediumLogo} className="md-logo" alt="OpenBox logo" />
+        <img src={SmallLogo} className="sm-logo" alt="OpenBox logo" />
         <ul className="navigation-links">
           <li>
             <Link to={"/"}>Accueil</Link>
@@ -26,6 +29,10 @@ export default function Header() {
       <div className="auth-links">
         <Link to={"/auth/login"}>Connexion</Link>
         <Link to={"/auth/register"}>Inscription</Link>
+      </div>
+
+      <div className="burger-menu">
+        <img src={BurgerIcon} className="sm-logo" alt="Burger Menu Icon" />
       </div>
     </div>
   );
