1cf6d4716e127f559a58effd81e56b61e0ea92b6
GDKobrae <djedjegboble.pro@gmail.com>
2023-11-18 20:47:41 +0100

    feat(footer): Add footer component  styles to root

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,8 @@ import { LinksFunction } from "@remix-run/node";
 
 import indexStyle from "./styles/index.css?url";
 import headerStyle from "./styles/header.css?url";
+import footerStyle from "./styles/footer.css?url";
+import Footer from "./ui/common/Footer/Footer";
 
 export const links: LinksFunction = () => [
   {
@@ -20,6 +22,10 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: headerStyle,
   },
+  {
+    rel: "stylesheet",
+    href: footerStyle
+  }
 ];
 
 export default function App() {
@@ -36,6 +42,7 @@ export default function App() {
         <ScrollRestoration />
         <LiveReload />
         <Scripts />
+        <Footer></Footer>
       </body>
     </html>
   );
