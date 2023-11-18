c07d36b2975bad1bdda422ef73516f40942f8d7b
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-18 11:28:43 +0100

    feat(header): added an object store for navigation links and mapped the render also started implementing the modalMenu states and component

--- app/ui/common/Header/Header.tsx
+++ app/ui/common/Header/Header.tsx
@@ -2,14 +2,31 @@ import { Link } from "@remix-run/react";
 import MediumLogo from "../../../assets/medium-logo.svg";
 import SmallLogo from "../../../assets/small-logo.svg";
 import BurgerIcon from "../../../assets/burger.svg";
+import { useState } from "react";
+import ModalMenu from "./ModalMenu";
 
 export default function Header() {
+  const [isModalActive, setIsModalActive] = useState(false);
+  const navigationLinks = {
+    Accueil: "/",
+    Calendrier: "/calendar",
+    OpenVote: "/openvote",
+    "Les Box": "/box",
+  };
+
   return (
     <div className="Header">
       <nav>
         <img src={MediumLogo} className="md-logo" alt="OpenBox logo" />
         <img src={SmallLogo} className="sm-logo" alt="OpenBox logo" />
         <ul className="navigation-links">
+          {Object.entries(navigationLinks).map(([pageName, pageUrl]) => {
+            return (
+              <li>
+                <Link to={pageUrl}>{pageName}</Link>
+              </li>
+            );
+          })}
           <li>
             <Link to={"/"}>Accueil</Link>
           </li>
@@ -31,9 +48,10 @@ export default function Header() {
         <Link to={"/auth/register"}>Inscription</Link>
       </div>
 
-      <div className="burger-menu">
+      <div className="burger-menu" onClick={() => setIsModalActive(true)}>
         <img src={BurgerIcon} className="sm-logo" alt="Burger Menu Icon" />
       </div>
+      {isModalActive && <ModalMenu />}
     </div>
   );
 }
