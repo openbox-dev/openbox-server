88c76f2b8ee1f695725a830746aec90666ea9fec
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-18 11:39:50 +0100

    feat(header): add ModalMenu component and framer motion implementation

---
+++ app/ui/common/Header/ModalMenu.tsx
@@ -0,0 +1,54 @@
+import { Link } from "@remix-run/react";
+import { motion } from "framer-motion";
+
+interface ModalMenuProps {
+  navigationLinks: Record<string, string>;
+  closeModal: () => void;
+}
+
+const containerVariants = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const navVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+export default function ModalMenu({
+  navigationLinks,
+  closeModal,
+}: ModalMenuProps) {
+  return (
+    <motion.div
+      className="ModalMenu"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <div className="close-menu" onClick={() => closeModal()}>
+        fermer
+      </div>
+      <motion.ul className="modalNavigationLinks">
+        {Object.entries(navigationLinks).map(([pageName, pageUrl]) => {
+          return (
+            <motion.li variants={navVariants}>
+              <Link to={pageUrl}>{pageName}</Link>
+            </motion.li>
+          );
+        })}
+      </motion.ul>
+    </motion.div>
+  );
+}
