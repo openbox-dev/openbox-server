1822e631c72867d029b25c8fa9681a71874c2e55
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-19 19:36:32 +0100

    fix(home): pass some styling from idnex to home && init new section

--- app/ui/pages/Home/Home.tsx
+++ app/ui/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import type { loader } from "~/routes/_index";
 import Header from "../../common/Header/Header";
 import SeeMore from "~/ui/common/SeeMore/SeeMore";
@@ -10,10 +10,10 @@ export default function Home() {
     <div className="Home">
       <section className="hero-banner">
         <div>
-          <h1>
+          <h1 className="section-title">
             OpenBox est un mouvement initié par des <span>Héticiens</span>
           </h1>
-          <p>
+          <p className="section-description">
             — Ce mouvement vise à faire grandir le savoir et le pouvoir d’agir
             de chaque Heticien grâce au partage, la rencontre et la découverte.
           </p>
@@ -26,16 +26,20 @@ export default function Home() {
         <h2>Nos évènements récents</h2>
         <div className="event-list"></div>
         {/* link */}
-        
+        <Link to={"/calendar"}>Voir le calendrier (flèche)</Link>
       </section>
 
       <section className="box-explain-section">
-        <h2>Qu'est ce qu'une box ?</h2>
-        <p>
-— Les Box sont des opportunités de découvrir des sujets atypiques, rencontrer des personnes ou s'améliorer sur un aspect particulier de la tech comme du quotidien. Elles se présentent sous la forme d’ateliers, de médias partagés, de rencontres, et plus encore.
-        </p>
+        <div>
+          <h2 className="section-title">Qu'est ce qu'une box ?</h2>
+          <p className="section-description">
+            — Les Box sont des opportunités de découvrir des sujets atypiques,
+            rencontrer des personnes ou s'améliorer sur un aspect particulier de
+            la tech comme du quotidien. Elles se présentent sous la forme
+            d’ateliers, de médias partagés, de rencontres, et plus encore.
+          </p>
+        </div>
       </section>
-
       {/* <ul>
         {articlesData.success
           ? articlesData.data.map((article) => {
