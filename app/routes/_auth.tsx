2f7d0274791f4e2e0117476c92ce857d6c4a070b
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-22 00:20:36 +0100

    fix(auth): add authStyle to layout component

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,5 +1,6 @@
 import { LinksFunction, MetaFunction } from "@remix-run/node";
 import AuthLayout from "~/ui/pages/Auth/AuthLayout";
+import authStyle from "../styles/auth.css?url";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,9 +13,11 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-// export const links: LinksFunction = () => {
-//   return [{ rel: "stylesheet", href: "" }];
-// };
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: authStyle,
+  },
+];
 
 export default AuthLayout;
-// export {};
