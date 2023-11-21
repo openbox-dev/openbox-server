26cfabdc47bc636ecfc6e3ce44b79f58532bb234
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-22 00:16:45 +0100

    feat(auth): improve templates for register and add template for Login, styles also

--- app/ui/pages/Auth/components/LoginForm.tsx
+++ app/ui/pages/Auth/components/LoginForm.tsx
@@ -1,3 +1,46 @@
+import { Form, Link } from "@remix-run/react";
+
+import arrowLink from "../../../../assets/icon/greenArrowLink.svg";
+import heticLogo from "../../../../assets/logo/hetic.svg";
+
 export default function LoginForm() {
-  return <div>login form</div>;
+  return (
+    <div className="LoginForm">
+      <h1>
+        Bienvenue sur <Link to={"/"}>OpenBox</Link>
+      </h1>
+      <Form method="post">
+        <label>
+          <span>Email</span>
+          <input
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="pietro@hetic.eu"
+          />
+        </label>
+        <label>
+          <span>Mot de passe</span>
+          <input
+            required
+            type="password"
+            name="password"
+            id="password"
+            placeholder="i0'c4xg9CqG}h!"
+          />
+        </label>
+        <button type="submit">S'inscrire</button>
+        <Link to="/register">
+          Je n'ai pas de compte
+          <img src={arrowLink} alt="Arrow icon" />
+        </Link>
+      </Form>
+      <span className="or-divider">OR</span>
+      <button className="Oauth-cta" disabled>
+        <img src={heticLogo} alt="Hetic Logo" />
+        Se connecter avec Hetic
+      </button>
+    </div>
+  );
 }
--- app/ui/pages/Auth/components/RegisterForm.tsx
+++ app/ui/pages/Auth/components/RegisterForm.tsx
@@ -1,35 +1,89 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useActionData } from "@remix-run/react";
+
+import arrowLink from "../../../../assets/icon/greenArrowLink.svg";
+import { action } from "~/routes/_auth.register";
+import { json } from "@remix-run/node";
 
 export default function RegisterForm() {
+  const formErrors = useActionData<typeof action>();
+
   return (
     <div className="RegisterForm">
-      <h1>Bienvenue sur OpenBox</h1>
+      <h1>
+        Bienvenue sur <Link to={"/"}>OpenBox</Link>
+      </h1>
       <Form method="post">
+        {formErrors && !formErrors.success ? (
+          typeof formErrors.data === "string" ? (
+            <span>{formErrors.data}</span>
+          ) : (
+            formErrors.data.map((error) => (
+              <span className="error-text">
+                {error.path === "promo"
+                  ? "Cette promo n'est pas valide"
+                  : error.message}
+              </span>
+            ))
+          )
+        ) : null}
         <label>
           <span>Email</span>
-          <input type="email" name="email" id="email" />
+          <input
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="pietro@hetic.eu"
+          />
         </label>
-        <label>
-          <span>Prénom</span>
-          <input type="text" name="firstName" id="firstName" />
-        </label>
-        <label>
-          <span>Nom</span>
-          <input type="text" name="lastName" id="lastName" />
-        </label>
-        <label>
-          <span>Promo</span>
-          <input type="text" name="promo" id="promo" />
-        </label>
+        <div className="user-data">
+          <label>
+            <span>Prénom</span>
+            <input
+              required
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="Pietro"
+            />
+          </label>
+          <label>
+            <span>Nom</span>
+            <input
+              required
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Schirano"
+            />
+          </label>
+          <label>
+            <span>Promo</span>
+            <input
+              required
+              type="text"
+              name="promo"
+              id="promo"
+              placeholder="WEB2"
+            />
+          </label>
+        </div>
         <label>
           <span>Mot de passe</span>
-          <input type="password" name="password" id="password" />
+          <input
+            required
+            type="password"
+            name="password"
+            id="password"
+            placeholder="i0'c4xg9CqG}h!"
+          />
         </label>
-        <button type="submit">go</button>
+        <button type="submit">S'inscrire</button>
+        <Link to="/login">
+          Je possède déjà un compte
+          <img src={arrowLink} alt="Arrow icon" />
+        </Link>
       </Form>
-      <Link to="/login">
-        <p>Je possède déjà un compte</p>
-      </Link>
     </div>
   );
 }
