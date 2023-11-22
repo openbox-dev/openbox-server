5df9fdfc9d6010f80390eece6c9318624b188b32
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-22 13:56:11 +0100

    feat(auth): add logout

---
+++ app/controllers/loaders/signout.loader.ts
@@ -0,0 +1,14 @@
+import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
+import { signOut } from "firebase/auth";
+import { SessionService } from "~/services/session.service";
+import { auth } from "~/utils/db-firebase";
+import { sessionStorage } from "~/utils/session.server";
+
+export async function signOutLoader({ request }: LoaderFunctionArgs) {
+  if (await SessionService.isTokenValid({ request })) {
+    await signOut(auth);
+    await SessionService.destroySession({ request }, "/");
+  }
+
+  return redirect("/login");
+}
