3c8cbf9ef91eb11d7e265304d7bf4aef9849d971
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-11-27 12:02:44 +0100

    fix: desktop and mobile animation

--- app/ui/pages/Auth/components/InfiniteBox.tsx
+++ app/ui/pages/Auth/components/InfiniteBox.tsx
@@ -1,68 +1,25 @@
-import BlueBox from "../../../../assets/logo/blue-box.svg";
-import RedBox from "../../../../assets/logo/red-box.svg";
-import BeigeBox from "../../../../assets/logo/beige-box.svg";
-import BrownBox from "../../../../assets/logo/brown-box.svg";
+import Boxes from "../../../../assets/logo/boxes.png";
 import { useEffect, useState } from "react";
 
-const boxes = [BlueBox, BrownBox, BeigeBox, RedBox];
-
 export default function InfiniteSlider() {
+  const boxesRowNumber = 5;
+
   return (
     <div className="InfiniteSlider">
       <div className="infinite-boxes-track">
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
+        <img src={Boxes} alt="Box Icon" />
       </div>
       <div className="infinite-boxes-track">
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
+        <img src={Boxes} alt="Box Icon" />
       </div>
       <div className="infinite-boxes-track">
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
+        <img src={Boxes} alt="Box Icon" />
       </div>
       <div className="infinite-boxes-track">
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
+        <img src={Boxes} alt="Box Icon" />
       </div>
       <div className="infinite-boxes-track">
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
-        {boxes.map((box, index) => {
-          return <img key={index} src={box} alt="Box Icon" />;
-        })}
+        <img src={Boxes} alt="Box Icon" />
       </div>
     </div>
   );
