11d69c4e40be21a2b932a8271856791e11ecc58a
Louisan Tchitoula <t.louisan@outlook.fr>
2023-11-27 13:52:36 +0100

    feat(box): init box loader in route and component

---
+++ app/controllers/loaders/nested.box.loader.ts
@@ -0,0 +1,5 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+
+export async function BoxLoader({ request }: LoaderFunctionArgs) {
+  return {};
+}
--- app/ui/pages/Nested/Box/Box.tsx
+++ app/ui/pages/Nested/Box/Box.tsx
@@ -1,3 +1,7 @@
+import { useLoaderData } from "@remix-run/react";
+import { loader } from "~/routes/_nested.box.$boxId";
+
 export default function Box() {
-  return <div>Page Box</div>;
+  const loaderData = useLoaderData<typeof loader>;
+  return <div className="Box">Page Box</div>;
 }
