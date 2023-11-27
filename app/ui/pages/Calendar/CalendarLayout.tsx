224fa8ed924a0bb4e566d55f590917bc75c67edf
GDKobrae <djedjegboble.pro@gmail.com>
2023-11-27 04:03:36 +0100

    feat(CalendarLayout):Add monthchange functionality

--- app/ui/pages/Calendar/CalendarLayout.tsx
+++ app/ui/pages/Calendar/CalendarLayout.tsx
@@ -1,27 +1,27 @@
+import { useState } from "react";
+
 const date = new Date();
 
 const month = date.getMonth();
+
 const monthDateOptions: Intl.DateTimeFormatOptions = { month: "long" };
-const monthInLetters: string = new Intl.DateTimeFormat(
-  "fr-Fr",
-  monthDateOptions
-).format(date);
+
+function getMonthInLetter(date: Date) {
+  let monthInLetters: string = date.toLocaleString("fr-Fr", monthDateOptions);
+
+  monthInLetters =
+    monthInLetters.charAt(0).toLocaleUpperCase() + monthInLetters.slice(1);
+
+  return monthInLetters;
+}
+
+const monthInLetters = getMonthInLetter(date);
 
 const weekDayOptions: Intl.DateTimeFormatOptions = { weekday: "long" };
-const weekDayInletters: string = new Intl.DateTimeFormat(
-  "fr-Fr",
-  weekDayOptions
-).format(date);
 
 const year = date.getFullYear();
 
 const numberOfEvent = 5;
-
-function getNumberOfDaysAMonth(year: number, month: number) {
-  return new Date(year, month + 1, 0).getDate();
-}
-
-const numberOfDaysAMonth = getNumberOfDaysAMonth(year, month);
 
 function getDaysInMonth(year: number, month: number) {
   const date = new Date(year, month, 1);
@@ -38,9 +38,68 @@ function getDaysInMonth(year: number, month: number) {
   return daysInMonth;
 }
 
+function convertMonthNumberToText(year: number, month: number) {
+  const date = new Date(year, month, 1);
+  let monthInLetters = date.toLocaleString("fr-FR", monthDateOptions);
+  monthInLetters =
+    monthInLetters.charAt(0).toLocaleUpperCase() + monthInLetters.slice(1);
+  return monthInLetters;
+}
+
 const daysInMonth = getDaysInMonth(year, month);
 
 export default function Calendar() {
+  const [monthNumber, setMonthNumber] = useState(month);
+  const [displayedMonth, setDisplayedMonth] = useState(monthInLetters);
+  const [displayedYear, setDisplayedYear] = useState(year);
+  const [displayedDaysInMonth, setDisplayedDaysInMonth] = useState(daysInMonth);
+
+  function handleNextButton() {
+    if (monthNumber < 11) {
+      const nextMonthNumber = monthNumber + 1;
+      const nextDaysInMonth = getDaysInMonth(displayedYear, nextMonthNumber);
+      const nextMonth = convertMonthNumberToText(
+        displayedYear,
+        nextMonthNumber
+      );
+      setMonthNumber(nextMonthNumber);
+      setDisplayedDaysInMonth(nextDaysInMonth);
+      setDisplayedMonth(nextMonth);
+    } else if (monthNumber === 11) {
+      const nextMonthNumber = 0;
+      const nextYear = displayedYear + 1;
+      const nextDaysInMonth = getDaysInMonth(nextYear, nextMonthNumber);
+      const nextMonth = convertMonthNumberToText(nextYear, nextMonthNumber);
+      setMonthNumber(nextMonthNumber);
+      setDisplayedDaysInMonth(nextDaysInMonth);
+      setDisplayedMonth(nextMonth);
+      setDisplayedYear(nextYear);
+    }
+  }
+
+  function handlePrevButton() {
+    if (monthNumber > 0) {
+      const nextMonthNumber = monthNumber - 1;
+      const nextDaysInMonth = getDaysInMonth(displayedYear, nextMonthNumber);
+      const nextMonth = convertMonthNumberToText(
+        displayedYear,
+        nextMonthNumber
+      );
+      setMonthNumber(nextMonthNumber);
+      setDisplayedDaysInMonth(nextDaysInMonth);
+      setDisplayedMonth(nextMonth);
+    } else if (monthNumber === 0) {
+      const nextMonthNumber = 11;
+      const nextYear = displayedYear - 1;
+      const nextDaysInMonth = getDaysInMonth(nextYear, nextMonthNumber);
+      const nextMonth = convertMonthNumberToText(nextYear, nextMonthNumber);
+      setMonthNumber(nextMonthNumber);
+      setDisplayedDaysInMonth(nextDaysInMonth);
+      setDisplayedMonth(nextMonth);
+      setDisplayedYear(nextYear);
+    }
+  }
+
   return (
     <div id="Calendar" className="Calendar">
       <div id="title-container" className="title-container">
@@ -51,11 +110,11 @@ export default function Calendar() {
         <aside id="month-information" className="month-information">
           <div id="text-container" className="text-container">
             <p id="year" className="date">
-              {year}
+              {displayedYear}
             </p>
 
             <p id="month" className="date">
-              {monthInLetters}
+              {displayedMonth}
             </p>
 
             <p id="nb-event" className="nb-event">
@@ -66,15 +125,19 @@ export default function Calendar() {
           </div>
 
           <div id="button-container" className="button-container">
-            <button className="button">Suivant</button>
-            <button className="button">Précédent</button>
+            <button className="button" onClick={handleNextButton}>
+              Suivant
+            </button>
+            <button className="button" onClick={handlePrevButton}>
+              Précédent
+            </button>
           </div>
 
           <div id="incoming-event" className="incoming-event"></div>
         </aside>
 
         <main id="calendar" className="calendar">
-          {daysInMonth.map((day) => {
+          {displayedDaysInMonth.map((day) => {
             return (
               <div className="calendar-day" key={day.day}>
                 <span>{day.day}</span>
